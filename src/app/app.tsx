372a920cf64a47e3c097c8c65b064dd37ea8ae60
shchansky <gavrilenko71285@gmail.com>
2022-12-22 22:08:14 +0300

    OOP praxis

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,6 +9,8 @@ function App() {
       <Pages.Timer />
       <hr />
       <Pages.FetchAbortController />
+      <hr />
+      <Pages.Oop/>
     </div>
   );
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from "./recursive-request";
 export * from "./components";
 export * from "./abort-controller";
+export * from "./oop";
