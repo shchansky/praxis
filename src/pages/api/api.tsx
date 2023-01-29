6a0de2b97c713249b48455af2ede394321a77e6c
shchansky <gavrilenko71285@gmail.com>
2023-01-29 21:25:29 +0300

    commit

--- src/pages/api/api.tsx
+++ src/pages/api/api.tsx
@@ -1,11 +1,24 @@
+import React from "react";
 import { FetchAbortController } from "./abort-controller";
 import { RecursiveRequest } from "./recursive-request";
 
-export const Api = () => (
-  <>
-    <FetchAbortController />
-    <hr />
-    <RecursiveRequest />
-    <hr />
-  </>
-);
+export const Api = () => {
+  const [state, setstate] = React.useState<any>(null);
+
+  React.useEffect(() => {
+    const res = fetch("http://localhost:3001/users").then((res) =>
+      res.json().then((res) => setstate(res))
+    );
+  }, []);
+
+  console.log(state);
+
+  return (
+    <>
+      <FetchAbortController />
+      <hr />
+      <RecursiveRequest />
+      <hr />
+    </>
+  );
+};
--- src/pages/oop/opp.tsx
+++ src/pages/oop/opp.tsx
@@ -8,30 +8,28 @@ class InputState {
     this.state = value;
   }
 
-  constructor() {
-
-  }
+  constructor() {}
 }
 
 export const Oop = () => {
+  const inputState = new InputState();
 
-   
+  const _inputValue = inputState.state;
+  const inputValue = React.useMemo(() => _inputValue, [_inputValue]);
 
+  console.log(inputState.state);
 
-    const inputState = new InputState()
-
-    const _inputValue = inputState.state
-    const inputValue = React.useMemo(()=>_inputValue, [_inputValue])
-
-    console.log(inputState.state)
-
- 
   return (
     <div>
       <h3 style={{ margin: "0", paddingBottom: "20px" }}>OOP</h3>
 
       <div>
-        <input value={inputValue} onChange={ev=>{inputState.setState(ev.target.value)}}/>
+        <input
+          value={inputValue}
+          onChange={(ev) => {
+            inputState.setState(ev.target.value);
+          }}
+        />
       </div>
     </div>
   );
